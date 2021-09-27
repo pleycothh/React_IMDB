b6aee693a31efd50c246d4375acb1e083cbe86ed
BenLi <zhuoheng.li@griffithuni.edu.au>
2021-09-27 13:11:44 +1000

    init commit for react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import profileImage from './images/no_image.jpg';
 
-function App() {
+//const Start = () => React.createElement('div', null, 'This is a little start')
+// use react function without JSX example above 
+
+
+
+function App () {
   return (
     <div className="App">
-      Start here.
+      <h1>My React App</h1>
+      <h2>Author: Ben Li</h2>
+      <img src={profileImage} alt="my-img" />
     </div>
   );
+ 
 }
 
 export default App;
