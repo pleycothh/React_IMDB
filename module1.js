e4508e6b850bdf91a94ba0d135ff463efc4ead14
Ben Li <ben.li19930119@gmail.com>
2021-11-10 01:27:41 +1000

    add npm example

---
+++ module1.js
@@ -0,0 +1,7 @@
+console.log('hi, module 1');
+
+var a = 1;
+console.log(a);
+
+var m2 = require('./module2');
+console.log(m2);
---
+++ module2.js
@@ -0,0 +1,8 @@
+console.log('hi, module 2');
+
+var a = 2;
+console.log(a);
+
+module.exports.a = a;
+module.exports.b = 2;
+exports.c = 3;
