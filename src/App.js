9d5f3467e4f313f24770ac895e881b68b1a7a41b
Ben Li <ben.li19930119@gmail.com>
2021-11-16 03:16:14 +1000

    add globalStyle

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import profileImage from './images/no_image.jpg';
 //const Start = () => React.createElement('div', null, 'This is a little start')
 // use react function without JSX example above 
 
-
+// Styles
+import {GlobalStype} from './GlobalStyle';
 
 function App () {
   return (
     <div className="App">
-      <h1>My React App test</h1>
+      Start here.
+      {/*<GlobalStype />*/}
+
+      <h1>My React App atest</h1>
       <h2>Author: Ben Li 19930119</h2>
       <img src={profileImage} alt="my-img" />
       
